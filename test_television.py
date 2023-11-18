0c40924627d91e08b9a50db28874b1f2cc498384
MasonWickstrom <mwickstrom@unomaha.edu>
2023-11-17 22:45:33 -0600

    Documentation and testing added

--- test_television.py
+++ test_television.py
@@ -20,7 +20,20 @@ class Test:
         assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
 
     def test_mute(self):
-        pass
+        self.tvl.power()
+        self.tvl.volume_up()
+        self.tvl.mute()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        
+        self.tvl.mute()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 1'
+        
+        self.tvl.power()
+        self.tvl.mute()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 1'
+
+        self.tvl.mute()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 1'
 
     def test_channel_up(self):
         self.tvl.channel_up()
@@ -43,10 +56,6 @@ class Test:
         self.tvl.channel_down()
         assert self.tvl.__str__() == 'Power = True, Channel = 3, Volume = 0'
 
-        self.tvl.channel_down()
-        self.tvl.channel_down()
-        assert self.tvl.__str__() == 'Power = True, Channel = 1, Volume = 0'
-
     def test_volume_up(self):
         self.tvl.volume_up()
         assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
@@ -55,19 +64,30 @@ class Test:
         self.tvl.volume_up()
         assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 1'
 
+        self.tvl.mute()
+        self.tvl.volume_up()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        
+        self.tvl.mute()
         self.tvl.volume_up()
         self.tvl.volume_up()
-        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 2'
-
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+        
     def test_volume_down(self):
         self.tvl.volume_down()
         assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
 
         self.tvl.power()
-        self.tvl.volume_down()
-        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
-
         self.tvl.volume_up()
         self.tvl.volume_up()
         self.tvl.volume_down()
         assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 1'
+
+        self.tvl.mute()
+        self.tvl.volume_down()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        self.tvl.mute()
+        self.tvl.volume_down()
+        self.tvl.volume_down()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 2'
